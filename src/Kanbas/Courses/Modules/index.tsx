72a60bed51400c47b52033e797e57322086e076c
jiaqiliu10 <156148621+jiaqiliu10@users.noreply.github.com>
2024-07-05 09:36:30 +0800

    Update index.tsx
    add button

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -3,7 +3,18 @@
 export default function Modules() {
   return (
     <div>
-      {/* Collapse All button, View Progress button, etc. */}
+      <div style={{ marginBottom: '5px', display: 'flex', gap: '5px' }}>
+        <button style={{ width: '90px', textAlign: 'center' }}>Collapse All</button>
+        <button style={{ width: '110px', textAlign: 'center' }}>View Progress</button>
+        <div style={{ width: '90px', textAlign: 'center' }}>
+          <select style={{ width: '100%' }}>
+            <option>Publish All</option>
+            <option>Option 1</option>
+            <option>Option 2</option>
+          </select>
+        </div>
+        <button style={{ width: '75px', textAlign: 'center' }}>+ Module</button>
+      </div>
       <ul id="wd-modules">
         <li className="wd-module">
           <div className="wd-title">Week 1</div>
