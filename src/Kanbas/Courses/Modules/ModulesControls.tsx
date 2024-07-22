ab13a42c7fe34fcce33b90b3a1a2cd05bda766da
jiaqiliu10 <liu.jiaqi10@northeastern.edu>
2024-07-22 21:29:21 +0800

    add icons

--- src/Kanbas/Courses/Modules/ModulesControls.tsx
+++ src/Kanbas/Courses/Modules/ModulesControls.tsx
@@ -10,7 +10,6 @@ export default function ModulesControls() {
             <div className="dropdown d-inline me-1 float-end">
                 <button id="wd-publish-all-btn" className="btn btn-lg btn-secondary dropdown-toggle"
                         type="button" data-bs-toggle="dropdown">
-
                     <GreenCheckmark/>
                     Publish All
                 </button>
