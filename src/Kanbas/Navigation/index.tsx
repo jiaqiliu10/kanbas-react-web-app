3c9fd4f93f1e6d0b550be66049bf062c09d900ca
jiaqiliu10 <liu.jiaqi10@northeastern.edu>
2024-07-22 23:50:52 +0800

    update

--- src/Kanbas/Navigation/index.tsx
+++ src/Kanbas/Navigation/index.tsx
@@ -12,14 +12,14 @@ export default function KanbasNavigation() {
     const { pathname } = useLocation();
     const links = [
         { label: "Dashboard", path: "/Kanbas/Dashboard", icon: AiOutlineDashboard },
-        { label: "Courses", path: "/Kanbas/Dashboard", icon: LiaBookSolid },
+        { label: "Courses", path: "/Kanbas/Courses", icon: LiaBookSolid },
         { label: "Calendar", path: "/Kanbas/Calendar", icon: IoCalendarOutline },
         { label: "Inbox", path: "/Kanbas/Inbox", icon: FaInbox },
         { label: "Labs", path: "/Labs", icon: LiaCogSolid },
     ];
 
     return (
-        <div id="wd-kanbas navigation" className="list-group rounded-0">
+        <div id="wd-kanbas-navigation" className="list-group rounded-0">
             <a id="wd-neu-link" target="=_blank"
                 href="https://www.northeastern.edu/"
                 className="list-group-item bg-black border-0">
@@ -39,34 +39,8 @@ export default function KanbasNavigation() {
                     {link.label}
                 </Link>
             ))}
-            {/* <a id='wd-account-link' href="#/Kanbas/Account"
-                className="list-group-item text-white
-                    bg-black text-center border-0 ">
-                <FaRegCircleUser className="fs-1 text text-white" /><br />
-                Account </a>
-            <a id="wd-dashboard-link" href="#/Kanbas/Dashboard"
-                className="list-group-item text-center border-0
-                   bg-white text-danger">
-                <AiOutlineDashboard className="fs-1 text-danger" /><br />
-                Dashboard </a>
-            <a id="wd-course-link" href="#/Kanbas/Courses"
-                className="list-group-item text-white
-                   bg-black text-center border-0">
-                <LiaBookSolid className="fs-1 text-danger" /><br />
-                Courses </a>
-            <a id='wd-calendar-link' href="#/Kanbas/Calendar"
-                className="list-group-item text-white bg-black text-center border-0">
-                <IoCalendarOutline className="fs-1 text-danger" /><br />
-                Calendar </a>
-            <a id='wd-inbox-link' href="#/Kanbas/Inbox"
-                className="list-group-item text-white bg-black text-center border-0">
-                <FaInbox className="fs-1 text-danger" /><br />
-                Inbox </a>
-            <a id="wd-labs-link" href="#/Labs"
-                className="list-group-item text-white bg-black text-center border-0">
-                <SlChemistry className="fs-1 text-danger" /><br />
-                Labs </a> */}
         </div>
     );
 }
+
 
