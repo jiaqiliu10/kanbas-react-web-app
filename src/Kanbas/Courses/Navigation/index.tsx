754a17b43086488c0e9b81d13b34fbbe3ae44493
jiaqiliu10 <liu.jiaqi10@northeastern.edu>
2024-07-22 22:22:40 +0800

    change some problems

--- src/Kanbas/Courses/Navigation/index.tsx
+++ src/Kanbas/Courses/Navigation/index.tsx
@@ -1,14 +1,17 @@
 import './index.css';
-import {Link, useLocation} from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
-export default function CoursesNavigation({courseId}: { courseId: string | undefined }) {
+export default function CoursesNavigation({ courseId }: { courseId: string | undefined }) {
     const links = ["Home", "Modules", "Piazza", "Zoom", "Assignments", "Quizzes", "Grades"];
-    const {pathname} = useLocation()
+    const { pathname } = useLocation();
     return (
         <div id="wd-courses-navigation" className="list-group fs-5 rounded-0">
             {links.map((link) => (
-                <Link key={link} to={`/Kanbas/Courses/${courseId}/${link}`} className={`list-group-item active border border-0 
-                ${pathname.includes(link) ? "text-danger" : "text-black"}`}> {link} </Link> ))}
+                <Link key={link} to={`/Kanbas/Courses/${courseId}/${link}`} className={`list-group-item border-0
+                    ${pathname.includes(link) ? "active" : ""}`}>
+                    {link}
+                </Link>
+            ))}
         </div>
     );
 }
--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -10,39 +10,33 @@ import { courses } from "../Database";
 import { FaAlignJustify } from "react-icons/fa";
 
 export default function Courses() {
-    const { cid} = useParams();
+    const { cid } = useParams();
     const course = courses.find((course) => course._id === cid);
     const { pathname } = useLocation();
-    const pathParts = pathname.split("/")
-
+    const pathParts = pathname.split("/");
 
     return (
         <div id="wd-courses">
             <h2 className="text-danger">
                 <FaAlignJustify className="me-4 fs-4 mb-1" />
                 {course && course.name} &gt; {pathParts[4]}
-                 {pathParts[5] && ` > ${pathParts[5]}`}
+                {pathParts[5] && ` > ${pathParts[5]}`}
             </h2>
-            <hr />
-            <table className={"w-100"}>
-                <tr>
-                    <td valign="top" style={{ width: "50px" }}>
-                        <CoursesNavigation courseId={cid} />
-                    </td>
-                    <td valign="top" >
-                        <Routes>
-                            <Route path="/" element={<Navigate to="Home" />} />
-                            <Route path="Home" element={<Home />} />
-                            <Route path="Modules" element={<Modules />} />
-                            <Route path="Assignments"
-                                element={<Assignments />} />
-                            <Route path="Assignments/:aid"
-                                element={<AssignmentEditor/>} />
-                            <Route path={"Grades"} element={<Grades />} />
-                        </Routes>
-                    </td>
-                </tr>
-            </table>
+            <div className="d-flex">
+                <div className="d-none d-md-block me-4">
+                    <CoursesNavigation courseId={cid} />
+                </div>
+                <div className="flex-fill">
+                    <Routes>
+                        <Route path="/" element={<Navigate to="Home" />} />
+                        <Route path="Home" element={<Home />} />
+                        <Route path="Modules" element={<Modules />} />
+                        <Route path="Assignments" element={<Assignments />} />
+                        <Route path="Assignments/:aid" element={<AssignmentEditor />} />
+                        <Route path="Grades" element={<Grades />} />
+                    </Routes>
+                </div>
+            </div>
         </div>
     );
 }
